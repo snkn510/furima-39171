50771a91b03df35f28d1a23fd97ab6f83dff499c
snkn510 <1192seina@gmail.com>
2023-04-17 22:21:23 +0900

    プルダウン実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_charge_id, :region_id, :days_to_ship_id, :price, :image)
+  end
+
 end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :region
+  belongs_to :days_to_ship
+
+  validates :title, :description, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
+
+
