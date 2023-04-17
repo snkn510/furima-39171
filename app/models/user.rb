ab97f938803b23c1e38cdf4d88ca3395b62b1896
snkn510 <1192seina@gmail.com>
2023-04-17 15:05:38 +0900

    コードレビュー後修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   with_options allow_blank: true do
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } do
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
       validates :first_name
       validates :last_name
     end
