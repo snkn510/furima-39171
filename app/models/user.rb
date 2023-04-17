3d63368d85cbe5cfc5fb7456fd0ff6ea36ef71bf
snkn510 <1192seina@gmail.com>
2023-04-17 12:25:15 +0900

    単体テストコード記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,24 @@ class User < ApplicationRecord
   validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
   validates :birthday,        presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
+  validates :password_confirmation, format: { with: VALID_PASSWORD_REGEX}
+
+
+  with_options allow_blank: true do
+
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
 
 
