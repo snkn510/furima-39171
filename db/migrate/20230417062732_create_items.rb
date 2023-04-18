12a2069853426b85a40127a0ae80d89c808d0aad
snkn510 <1192seina@gmail.com>
2023-04-18 16:17:54 +0900

    コードレビュー後修正②

--- db/migrate/20230417062732_create_items.rb
+++ db/migrate/20230417062732_create_items.rb
@@ -1,18 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :title
-      t.text :description
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :shipping_charge_id
-      t.integer :region_id
-      t.integer :days_to_ship_id
-      t.integer :price
-      t.references :user
+      t.string :title,               null: false
+      t.text :description,           null: false
+      t.integer :category_id,        null: false
+      t.integer :condition_id,       null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :region_id,          null: false
+      t.integer :days_to_ship_id,    null: false
+      t.integer :price,              null: false
+      t.references :user,            null: false, foreign_key: true 
       t.timestamps
     end
   end
 end
-
-
