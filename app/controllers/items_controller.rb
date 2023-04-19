90616a10e9c61c9357a85019c6155add98c1ab5c
snkn510 <1192seina@gmail.com>
2023-04-19 15:51:17 +0900

    詳細画面表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_charge_id, :region_id,
-                                 :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_charge_id, :region_id,:days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
