6a591821228560e4c5a9ab3c7d6e168ef1689b61
snkn510 <1192seina@gmail.com>
2023-04-23 22:30:47 +0900

    Formオブジェクト

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,44 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    @order_address = OrderAddress.create(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+   else
+     render action: :index
+   end
+  end
+  
+  private
+  
+  def order_params
+    params.permit(:postal_code, :region_id, :city, :street, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+end
+
+
+# @order_form = OrderForm.new(order_params)
+# if @order_form.valid?
+#   pay_item
+#   @order_form.save
+#   redirect_to root_path
+# else
+#   render :index
+# end
+# end
+
+# private
+
+# def order_params
+# # この時点では、order_idが不要。またrequire外の情報は参照するため、mergeとする。
+# params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+# end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,26 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :region_id, :city, :street, :building_name, :phone_number, :order_id
+  # order_idは、保存されたタイミングで生成されるため、attr_accessorにおいて不要なカラムとなる（書くと蛇足なのでエラー）
+  attr_accessor :user_id, :item_id, :postal_code, :region_id, :city, :street, :building_name, :phone_number
 
+ # 4行目と同じくこのタイミングでは生成前なので「validates :order_id」は不要
   with_options presence: true do
+    # orderモデルのバリデーション
     validates :user_id
     validates :item_id
+    # addressモデルのバリデーション
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :city, allow_blank: true
-    validates :street, allow_blank: true
-    validates :phone_number
-    validates :order_id
+    validates :city
+    validates :street
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
   end
 
-  
-
+  def save
+    # 寄付情報を保存し、変数donationに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    # order_idには、変数orderのidと指定する
+    Address.create(postal_code: postal_code, region_id: region_id, city: city, street: street, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
